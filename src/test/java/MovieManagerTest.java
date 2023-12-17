71df33e09c90af346bd8513aef5e27dcb77fa550
Grigoriy Kaygorodtsev <grigoriy.kaygorotsev@gmail.com>
2023-12-17 14:24:35 +0300

    Initial commit

---
+++ src/test/java/MovieManagerTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MovieManagerTest {
+
+
+    @Test
+    public void test() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Film I");
+        manager.addMovie("Film II");
+        manager.addMovie("Film III");
+        manager.addMovie("Film IV");
+        manager.addMovie("Film V");
+        manager.addMovie("Film VI");
+        manager.addMovie("Film VII");
+        String[] expected = {"Film I", "Film II", "Film III", "Film IV", "Film V", "Film VI", "Film VII"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test2() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Film I");
+        manager.addMovie("Film II");
+        manager.addMovie("Film III");
+        manager.addMovie("Film IV");
+        manager.addMovie("Film V");
+
+        String[] expected = {"Film V", "Film IV", "Film III", "Film II", "Film I"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
